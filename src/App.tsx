baa75baebba320aa178f1a1bfc63afdb24a9c7ab
BensonB12 <bensonbird2@gmail.com>
2025-03-01 00:58:00 -0700

    CPU-Centered

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Layout } from "./Layout";
+import cpuImage from "./assets/cpu-bolt-svgrepo-com.svg";
 
 export const App = () => {
   return (
     <Layout>
-      <div></div>
+      <div className="h-100 d-flex justify-content-center align-items-center">
+        <div className="h-25 d-flex justify-content-center align-items-center">
+          <img className="h-25" src={cpuImage} alt="CPU" />
+        </div>
+      </div>
     </Layout>
   );
 };
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="d-flex bg-dark min-vh-100">
+    <div className="d-flex bg-dark vh-100">
       <div className="flex-grow-1">{children}</div>
     </div>
   );
